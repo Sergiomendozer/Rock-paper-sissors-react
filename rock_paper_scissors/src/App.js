adc3a931d25997daf165724cef570aea0cfb199e
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-26 01:56:10 -0800

    mostly formated almost done

--- rock_paper_scissors/src/App.js
+++ rock_paper_scissors/src/App.js
@@ -5,20 +5,34 @@ function App() {
     <div>
       <h1 className="title">Rock Paper Scissors</h1>
       <p1 className="prompt_user_to_choose">Select your attack: </p1>
-      <button type="button" class="btn btn-primary btn-lg">
+      <button type="button" className="Rock">
         Rock
       </button>
       <p-space> </p-space>
-      <button type="button" class="btn btn-primary btn-lg">
+      <button type="button" className="Paper">
         Paper
       </button>
       <p-space> </p-space>
-      <button type="button" class="btn btn-primary btn-lg">
+      <button type="button" className="Scissors">
         Scissors
       </button>
       <div>
         <p-space> </p-space>
         <p2 className="Bobs_choice">Bob has chosen:</p2>
+        <button type="Label" class="btn-circle btn-xl">
+          test
+        </button>
+      </div>
+      <div>
+        <p3 className="results">Result:</p3>
+      </div>
+      <div>
+        <p3 className="results">Winner is:</p3>
+      </div>
+      <div>
+        <button type="button" className="new_game">
+          New Game
+        </button>
       </div>
     </div>
   );
