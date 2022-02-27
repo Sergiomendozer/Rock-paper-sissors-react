ad8c099d918d3fda19e01e25c1d31a3d0f6d2654
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-27 00:08:25 -0800

    attack buttons functions made

--- rock_paper_scissors/src/attack_buttons/click_scissors.js
+++ rock_paper_scissors/src/attack_buttons/click_scissors.js
@@ -1,4 +1,6 @@
 import react from "react";
+import random_attack_choice from "../bob's_choice/choice";
+
 var user_picked_attack = " ";
 export default function Click_scissors() {
   user_picked_attack = "Scissors";
