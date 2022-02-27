965eda5bceba46bb82ba865ff6ac4d6c737b9d8e
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-27 01:40:28 -0800

    neew funciton

--- rock_paper_scissors/src/bob's_choice/choice.js
+++ rock_paper_scissors/src/bob's_choice/choice.js
@@ -1,9 +1,10 @@
 import react from "react";
 import ReactDOM from "react-dom";
+import who_won from "./who_won";
 
 var bobs_attack_choice = " ";
 
-export default function random_attack_choice() {
+export default function random_attack_choice(user_picked_attack) {
   var list_of_choices = ["Rock", "Paper", "Scissors"];
   bobs_attack_choice =
     list_of_choices[Math.floor(Math.random() * list_of_choices.length)];
@@ -12,5 +13,5 @@ export default function random_attack_choice() {
     <p>{bobs_attack_choice}</p>,
     document.getElementById("bobs_attack_choice")
   );
-  return bobs_attack_choice;
+  who_won(user_picked_attack, bobs_attack_choice);
 }
---
+++ rock_paper_scissors/src/bob's_choice/who_won.js
@@ -0,0 +1,15 @@
+import react from "react";
+import ReactDOM from "react-dom";
+
+export default function who_won(user_picked_attack, bobs_attack_choice) {
+  console.log(`users choice:{user_picked_attack}`);
+  console.log(`Bob's choice:{bobs_attack_choice}`);
+
+  if (user_picked_attack === "Rock" && bobs_attack_choice === "Rock") {
+    console.log("tie");
+  } else if (user_picked_attack == "Rock" && bobs_attack_choice == "Paper") {
+    console.log("Bob wins");
+  } else if (user_picked_attack == "Rock" && bobs_attack_choice == "Scissors") {
+    console.log("You win");
+  } else console.log("none");
+}
